e79831e46a738408b5024391c1cba9a39ac731f6
Vityan <vgusarik07@gmail.com>
2018-03-01 00:54:27 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,37 @@
+﻿import config
+import telebot
+import botan
+import random
+import weather
+bot = telebot.TeleBot(config.token)
+random.seed()
+
+
+@bot.message_handler(commands=['random'])
+def cmd_random(message):
+    bot.send_message(message.chat.id, random.randint(1, 10))
+    # Если не нужно собирать ничего, кроме количества использований, замените третий аргумент message на None
+    botan.track(config.botan_key, message.chat.id, message, 'Случайное число')
+    return
+
+
+@bot.message_handler(commands=['yesorno'])
+def cmd_yesorno(message):
+    bot.send_message(message.chat.id, random.choice(strings))
+    # Если не нужно собирать ничего, кроме количества использований, замените третий аргумент message на None
+    botan.track(config.botan_key, message.chat.id, message, 'Да или нет')
+    return
+
+
+@bot.message_handler(commands=['weather'])
+def cmd_weather(message):
+    string_weather = weather.getweather('севастополь')
+    bot.send_message(message.chat.id, string_weather)
+    botan.track(config.botan_key, message.chat.id, message, 'Погода')
+    return
+
+if __name__ == '__main__':
+    global strings
+    strings = ['Да', 'Нет']
+    bot.polling(none_stop=True)
+
---
+++ config.py
@@ -0,0 +1,2 @@
+token = '536521993:AAFAy_rZ48FK14_ZRc9EG_7oak3xUdkV_XA'
+botan_key= '153fc0a1-42ab-4d36-820b-8e5e7beb6512'
