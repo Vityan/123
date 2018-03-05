d9d5fd4eb15f7b11742321a0b95355cff868c9ce
Vityan <36860352+Vityan@users.noreply.github.com>
2018-03-05 15:37:34 +0000

    ABORT

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import weather
 bot = telebot.TeleBot(config.token)
 random.seed()
 
-city ="";
+
 
 @bot.message_handler(commands=["start"]) # Обработка /start
 def handle_start(message):
@@ -28,18 +28,14 @@ def cmd_yesorno(message):
     botan.track(config.botan_key, message.chat.id, message, 'Да или нет')
     return
 
-def handle_text(message):
-    if message.text[:7] == "Погода " or message.text[:7] == "погода " :
-        city=message.text[8:]
-    bot.send_message('Погода catched')
-    return
+
         
         
     
 
 @bot.message_handler(commands=['weather'])
 def cmd_weather(message):
-    string_weather = weather.getweather(city)
+    string_weather = weather.getweather('Севастополь')
     bot.send_message(message.chat.id, string_weather)
     botan.track(config.botan_key, message.chat.id, message, 'Погода')
     return
