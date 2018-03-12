4a812110bf4b22ba5c1c3758a73e44d30b6e7cbd
Vityan <36860352+Vityan@users.noreply.github.com>
2018-03-12 16:06:12 +0000

    xfybc

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def cmd_yesorno(message):
 
 @bot.message_handler(content_types=['text'])
 def hand_text(message):
-   if messege.text == "/weather":	
+   if message.text == "/weather":	
       string_weather = weather.getweather('Севастополь')
       bot.send_message(message.chat.id, string_weather)
       botan.track(config.botan_key, message.chat.id, message, 'Погода')
