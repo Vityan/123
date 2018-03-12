80e4f5a0da71fa87bb7b0c0d9bab5110396bd6d7
Vityan <36860352+Vityan@users.noreply.github.com>
2018-03-12 13:47:43 +0000

    answer text

--- bot.py
+++ bot.py
@@ -6,24 +6,26 @@ import weather
 bot = telebot.TeleBot(config.token)
 random.seed()
 
+@bot.message_handler(commands=["start"]) # Обработка /start
+def handle_start(message):
+    bot.send_message(message.from_user.id, 'Привет  \nMy dear friend ')
+
+
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
 
 # Если пользователь отправил "привет, как тебя зовут?" отвечаем "робот я"
-if message.text == "привет, как тебя зовут?":
-   bot.send_message(message.from_user.id, 'робот я'
+   if message.text == "привет, как тебя зовут?":
+      bot.send_message(message.from_user.id, 'робот я')
 
 # Если пользователь отправил "и чо?" отвечаем "да ничо"
-elif message.text == "и чо?":
-   bot.send_message(message.from_user.id, 'да ничо'
+   elif message.text == "и чо?":
+      bot.send_message(message.from_user.id, 'да ничо')
 
 #Если пользователь отправил слово/фразу, на которое(ую) нет ответа
-else:
-   bot.send_message(message.from_user.id, "Извините, я Вас не понимаю"
+   else:
+      bot.send_message(message.from_user.id, "Извините, я Вас не понимаю")
 
-@bot.message_handler(commands=["start"]) # Обработка /start
-def handle_start(message):
-    bot.send_message(message.from_user.id, 'Привет  \nMy ')
 
 
 @bot.message_handler(commands=['random'])
